91975be4567f8faeaa1a391ede04622129c19820
user name <user email>
2021-01-15 14:58:38 +0530

    kre

--- net/settings.py
+++ net/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
